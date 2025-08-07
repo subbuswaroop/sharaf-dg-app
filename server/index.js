d3f8a610e54857011ebbcaa4260eecbfea1255ff
subbuswaroop <subbuswaroop3795@gmail.com>
2025-08-07 10:21:42 +0530

    Config change

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 5000
 // Middleware
 app.use(helmet())
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+  origin: process.env.FRONTEND_URL || 'http://localhost:3333',
   credentials: true
 }))
 app.use(express.json())
